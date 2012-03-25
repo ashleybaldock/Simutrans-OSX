2c9b22ce9bda8b197565ccd739adb0d32b401aa3
Timothy Baldock <tb@entropy.me.uk>
2012-03-25 21:43:53 +0100

    Add entitlements file for testing that subsystem Add OSX specific simsys file, to contain the methods we need on this platform

---
+++ simsys_osx.mm
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2011-2012 Simutrans Project
+ *
+ * Created by Timothy Baldock on 25/03/2012.
+ *
+ * This file is part of the Simutrans project under the artistic licence.
+ */
+
+/*
+ * This file provides OSX specific functionality, use instead of simsys.cc
+ * Provides support for application sandboxing
+ */
+
+#include "macros.h"
+#include "simmain.h"
+#include "simsys.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+
+
+#include <limits.h>
+#include <locale.h>
+
+struct sys_event sys_event;
+
+
+void dr_mkdir(char const* const path)
+{
+	mkdir(path, 0777);
+}
+
+
+char const* dr_query_homedir()
+{
+	static char buffer[PATH_MAX];
+	
+	sprintf(buffer, "%s/Library/Containers/org.simutrans.simutrans/Data/Library/Application Support", getenv("HOME"));
+	
+	// create other subdirectories
+	strcat(buffer, "/");
+	
+	char b2[PATH_MAX];
+	sprintf(b2, "%smaps", buffer);
+	dr_mkdir(b2);
+	sprintf(b2, "%ssave", buffer);
+	dr_mkdir(b2);
+	sprintf(b2, "%sscreenshot", buffer);
+	dr_mkdir(b2);
+	
+	return buffer;
+}
+
+
+/*
+ * This retrieves the 2 byte string for the default language
+ */
+
+
+const char *dr_get_locale_string()
+{
+	static char code[4];
+	char *ptr;
+	setlocale( LC_ALL, "" );
+	ptr = setlocale( LC_ALL, NULL );
+	code[0] = 0;
+	for(  int i=0;  i<lengthof(code)-1  &&  isalpha(ptr[i]);  i++  ) {
+		code[i] = tolower(ptr[i]);
+		code[i+1] = 0;
+	}
+	setlocale( LC_ALL, "C" );	// or the numberourpur may be broken
+	return code[0] ? code : NULL;
+}
+
+
+
+void dr_fatal_notify(char const* const msg)
+{
+	// TODO - show message box in app
+	fputs(msg, stderr);
+}
+
+
+int sysmain(int const argc, char** const argv)
+{
+#if !defined __BEOS__
+#	if defined __GLIBC__ && !defined __AMIGA__
+	/* glibc has a non-standard extension */
+	char* buffer2 = 0;
+#	else
+	char buffer2[PATH_MAX];
+#	endif
+#	ifndef __AMIGA__
+	char buffer[PATH_MAX];
+	ssize_t const length = readlink("/proc/self/exe", buffer, lengthof(buffer) - 1);
+	if (length != -1) {
+		buffer[length] = '\0'; /* readlink() does not NUL-terminate */
+		argv[0] = buffer;
+	}
+#	endif
+	// no process file system => need to parse argv[0]
+	/* should work on most unix or gnu systems */
+	argv[0] = realpath(argv[0], buffer2);
+#endif
+	
+	return simu_main(argc, argv);
+}
